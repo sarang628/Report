126a8b70a6206929cae20296682abe495ed31713
SRYANG <sarang628@naver.com>
2023-11-22 13:27:17 +0800

    update

---
+++ library/src/main/java/com/sryang/torang/report/compose/ReportNavHost.kt
@@ -0,0 +1,33 @@
+package com.sryang.torang.report.compose
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun ReportNavHost() {
+    val navHost = rememberNavController()
+    NavHost(navController = navHost, startDestination = "reason") {
+        composable("reason") {
+            ReportScreen(onReport = {
+                navHost.navigate("thanksForLettingUsKnow")
+            })
+        }
+        composable("thanksForLettingUsKnow") {
+            ThanksForLettingUsKnow()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun PreviewReportNavHost() {
+    ModalBottomSheet(onDismissRequest = { /*TODO*/ }) {
+        ReportNavHost()
+    }
+}
---
+++ library/src/main/java/com/sryang/torang/report/compose/SupportActions.kt
@@ -0,0 +1,32 @@
+package com.sryang.torang.report.compose
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun SupportActions() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text(text = "Support actions") }, navigationIcon = {
+                Icon(imageVector = Icons.Outlined.ArrowBack, contentDescription = "")
+            })
+        }
+    ) { paddingValues ->
+        Column(Modifier.padding(paddingValues)) {
+
+        }
+    }
+}
