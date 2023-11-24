fbb47d96158af676d7048679f095fd844f6bb94d
SRYANG <sarang628@naver.com>
2023-11-24 12:25:10 +0800

    update git ignore

---
+++ app/src/main/java/com/sryang/torang/report/test/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sryang.torang.report.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
